e2b75b0f8ba0c606291b0c5f7d7a675ffd74d3bb
Mpendulo <mpendulo.dev@gmail.com>
2022-06-06 20:37:16 +0200

    changed and fixed errors

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,18 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+  proxy: true,
+  url: env("https://tsenga-content.herokuapp.com/"),
+  app: {
+    keys: [
+      "nAMUlTADx9FJkYZVpdtqbg==",
+      "5sWmv/mJ1i2P1uuKUqKRxg==",
+      "OxkEkP4zkP4k4Eg4KGeD9g==",
+      "CYtndTpPJHgdjBFxO0niNQ==",
+    ],
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "WanoToOmG4HA4uiVUoBd4Q",
+  },
+  auth: {
+    secret: "GMi2G6oCB0Y0yt1ebCXICA",
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,8 +2,17 @@ module.exports = ({ env }) => ({
   proxy: true,
   url: env("https://tsenga-content.herokuapp.com/"),
   app: {
-    keys: env.array(
-      "nAMUlTADx9FJkYZVpdtqbg==,5sWmv/mJ1i2P1uuKUqKRxg==,OxkEkP4zkP4k4Eg4KGeD9g==,CYtndTpPJHgdjBFxO0niNQ=="
-    ),
+    keys: [
+      "nAMUlTADx9FJkYZVpdtqbg==",
+      "5sWmv/mJ1i2P1uuKUqKRxg==",
+      "OxkEkP4zkP4k4Eg4KGeD9g==",
+      "CYtndTpPJHgdjBFxO0niNQ==",
+    ],
+  },
+  apiToken: {
+    salt: "WanoToOmG4HA4uiVUoBd4Q",
+  },
+  auth: {
+    secret: "GMi2G6oCB0Y0yt1ebCXICA",
   },
 });
