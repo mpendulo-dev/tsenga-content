3230a236de46eda35a5970a1be75ca2dcd331dd5
Mpendulo <mpendulo.dev@gmail.com>
2022-06-06 19:59:52 +0200

    Update database config

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  proxy: true,
+  url: env("https://tsenga-content.herokuapp.com/"),
+  app: {
+    keys: env.array(
+      "nAMUlTADx9FJkYZVpdtqbg==,5sWmv/mJ1i2P1uuKUqKRxg==,OxkEkP4zkP4k4Eg4KGeD9g==,CYtndTpPJHgdjBFxO0niNQ=="
+    ),
+  },
+});
